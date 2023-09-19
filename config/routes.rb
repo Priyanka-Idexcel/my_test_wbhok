b01c8f287f96208201a1845c97d552bc776338da
Priyanka k <priyanka.k@idexcel.com>
2023-09-19 23:25:11 +0530

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'webhooks#receive'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+   post '/webhooks/receive', to: 'webhooks#receive'
 end
